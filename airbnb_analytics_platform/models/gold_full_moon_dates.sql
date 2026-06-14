194e58b884ad2e6dff81845e2e9e7b1f8b4b7adb
Nassima DHAIMAN <nassimadhaiman@gmail.com>
2026-06-14 20:52:47 +0200

    gold data models

--- airbnb_analytics_platform/models/gold_full_moon_dates.sql
+++ airbnb_analytics_platform/models/gold_full_moon_dates.sql
@@ -1,49 +1,38 @@
-WITH reviews_with_flag AS (
+WITH base_reviews AS (
     SELECT
         r.reviewer_name,
         r.review_date,
         r.sentiment,
-        r.listing_id,
-
-        CASE
-            WHEN EXISTS (
-                SELECT 1
-                FROM {{ ref('silver_full_moon_dates') }} fm
-                WHERE r.review_date BETWEEN
-                    (fm.full_moon_date - INTERVAL '3 days')
-                    AND
-                    (fm.full_moon_date + INTERVAL '3 days')
-            ) THEN TRUE
-            ELSE FALSE
-        END AS is_near_full_moon
-
+        r.listing_id
     FROM {{ ref('silver_reviews') }} r
     WHERE r.review_date IS NOT NULL
       AND r.sentiment IN ('positive', 'negative', 'neutral')
+),
+
+moon_flag AS (
+    SELECT
+        br.*,
+        CASE 
+            WHEN fm.full_moon_date IS NOT NULL THEN 1
+            ELSE 0
+        END AS is_near_full_moon
+    FROM base_reviews br
+    LEFT JOIN {{ ref('silver_full_moon_dates') }} fm
+        ON br.review_date BETWEEN (fm.full_moon_date - INTERVAL '3 days')
+                              AND (fm.full_moon_date + INTERVAL '3 days')
 )
 
 SELECT
     is_near_full_moon,
     sentiment,
-
     COUNT(*) AS nb_reviews,
 
     ROUND(
-        CASE
-            WHEN SUM(COUNT(*)) OVER (PARTITION BY is_near_full_moon) = 0 THEN 0
-            ELSE (
-                COUNT(*) * 100.0
-                / SUM(COUNT(*)) OVER (PARTITION BY is_near_full_moon)
-            )
-        END,
-    1) AS pct_within_period
+        COUNT(*) * 100.0 /
+        SUM(COUNT(*)) OVER (PARTITION BY is_near_full_moon),
+    1) AS sentiment_share_pct
 
-FROM reviews_with_flag
+FROM moon_flag
 
-GROUP BY
-    is_near_full_moon,
-    sentiment
-
-ORDER BY
-    is_near_full_moon,
-    nb_reviews DESC
+GROUP BY is_near_full_moon, sentiment
+ORDER BY is_near_full_moon, nb_reviews DESC;
